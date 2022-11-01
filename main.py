e0fc61a71dcb3875efbda274e18e5eab393e468c
53142 <89957019+53142@users.noreply.github.com>
2022-11-01 12:15:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ root.title("Pomodoro App")
 root.geometry("500x500")
 root.state("zoomed")
 
+# Define document styles
+style = ttk.Style()
+style.map("C.TButton",
+    foreground=[('pressed', 'red'), ('active', 'blue')],
+    background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+    )
 
 # Code to add widgets will go here...
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="PomoImprove").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frm, text="Quit", style="C.TButton", command=root.destroy).grid(column=1, row=0)
 
 root.mainloop()
