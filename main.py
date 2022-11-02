299b29e0b133486a9399c81fa6b1039dd0ef4ad4
53142 <89957019+53142@users.noreply.github.com>
2022-11-02 11:38:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ root = Tk()
 # Adjust window settings
 root.configure(background="#AEAEAE")
 root.title("Pomodoro App")
+# For setting the window size to this on window unmaxize
 root.geometry("500x500")
 root.state("zoomed")
 
@@ -15,10 +16,20 @@ style.map("C.TButton",
     background=[('pressed', '!disabled', 'black'), ('active', 'white')]
     )
 
+# Functions
+def open_popup():
+   top= Toplevel(root)
+   top.geometry("750x250")
+   top.title("Child Window")
+   Label(top, text= "Hello World!", font=('Mistral 18 bold')).place(x=150,y=80)
+
 # Code to add widgets will go here...
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="PomoImprove").grid(column=0, row=0)
+ttk.Label(frm, text="PomoImprove", font=('Mistral 18 bold')).grid(column=0, row=0)
+
+ttk.Button(frm, text="test popup", style="C.TButton", command=open_popup).grid(column=1, row=2)
 ttk.Button(frm, text="Quit", style="C.TButton", command=root.destroy).grid(column=1, row=0)
+
 
 root.mainloop()
