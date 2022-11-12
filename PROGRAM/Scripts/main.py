2ed5be9605d02a659a836905f5ddcd9a50ad77ac
53142 <89957019+53142@users.noreply.github.com>
2022-11-11 19:32:06 -0700

    many things
    many things. I updated main.py and added 1 line to README

--- PROGRAM/Scripts/main.py
+++ PROGRAM/Scripts/main.py
@@ -1,32 +1,41 @@
 from tkinter import ttk
 from tkinter import *
-from tkinter import messagebox
 import time
 import lockdown
+
 
 root = Tk()
 
 # Adjust window settings
-root.configure(background="#F0F0F0")
+root.configure(background="#AEAEAE")
 root.title("Pomoimprove")
 root.resizable(False,False)
 root.overrideredirect(True)
 # Set window to be maximized by default
 root.state("zoomed")
 
+
+
+
 # Define document styles
 style=ttk.Style()
 style.map("C.TButton",
-    foreground=[('pressed', 'red'), ('active', 'blue')],
+    foreground=[('pressed', 'white'), ('active', 'black')],
     background=[('pressed', '!disabled', 'black'), ('active', 'white')]
     )
 
 # Functions
-def open_popup():
-   top=Toplevel(root)
-   top.geometry("750x250")
-   top.title("Child Window")
-   Label(top, text="Hello World!", font=('Mistral 18 bold')).place(x=150,y=80)
+
+def countdown(count, start):
+    # change text in label
+    label['text'] = count
+
+    if count > 0 & start == True:
+        # call countdown again after 1000ms (1s)
+        root.after(1000, countdown, count-1)
+    elif count == 0 or start == False:
+        label['text'] = "DONE!"
+
 
 # Code to add widgets will go here...
 frm = ttk.Frame(root, padding=10)
@@ -35,54 +44,62 @@ frm.grid()
 
 
 # Specify Grid
-Grid.rowconfigure(root,0,weight=1)
-Grid.columnconfigure(root,0,weight=1)
+Grid.rowconfigure(root, 0, weight=1)
+Grid.columnconfigure(root, 0, weight=1)
  
-Grid.rowconfigure(root,1,weight=1)
+Grid.rowconfigure(root, 1, weight=1)
  
 # Create Buttons
-pomodoros = Label(root,text="Pomdoros")
-button_2 = Button(root,text="Settings")
+pomodoros = Label(root, text="Pomdoros")
+button_2 = Button(root, borderwidth=0, text="Settings")
  
 # Set grid
 # Stick to left side
-pomodoros.grid(row=0,column=0,sticky="NSW")
-button_2.grid(row=3,column=0,sticky="NSW")
+pomodoros.grid(row=0, column=0, sticky="NSW")
+button_2.grid(row=3, column=0, sticky="NSW")
 
 
 
 
 
-
-Label(root, text="Pomoimprove", font=('Helvetica 25 bold')).place(relx=.5, rely=.5,anchor=CENTER)
-
-ttk.Button(frm, text="test popup", style="C.TButton", command=open_popup).grid(column=1, row=2)
-ttk.Button(frm, text="Quit", style="C.TButton", command=root.destroy).grid(column=1, row=0)
+# Main Pomoimprove Logo
+Label(root, text="Pomoimprove", font=('Helvetica 25 bold'), bg="#AEAEAE").place(relx=.5, rely=.05, anchor=CENTER)
 
 
+# Timer length input boxes
 
+hours=IntVar()
+minutes=IntVar()
 
+#Minutes = Entry(root, bg="#AEAEAE").place(relx=.53, rely=.35, relheight=.02, relwidth=.05, anchor=CENTER)
 
+spinbox = Spinbox(root, bg="#AEAEAE", from_=0, to=60, increment=5, wrap=True, justify=CENTER, textvariable=minutes).place(relx=.53, rely=.25, relheight=.02, relwidth=.025, anchor=CENTER)
 
-
-def countdown(count):
-    # change text in label        
-    label['text'] = count
-
-    if count > 0:
-        # call countdown again after 1000ms (1s)
-        root.after(1000, countdown, count-1)
-    elif count == 0:
-        label['text'] = "DONE!"
-
-
-
-label = Label(root)
-label.place(x=35, y=15)
-
-# call countdown first time    
-countdown(5)
+# call countdown first time
 # root.after(0, countdown, 5)
 
-root.after(500,lockdown.startlockdown)
+# Needs to be before start/stop timer buttons
+label = Label(root, text="test")
+label.place(x=35, y=15)
+
+
+
+# Make the start/stop timer buttons
+Start = Button(root, bg="#3CC249", fg="#FFFFFF", borderwidth=0, text="Start", activeforeground="black", command = countdown(3, True)).place(relx=.50, rely=.80, relheight=.03, relwidth=.05, anchor=CENTER)
+Stop = Button(root, bg="#E33E3E", fg="#FFFFFF", borderwidth=0, text="Stop", activeforeground="black", command = countdown(3, False)).place(relx=.50, rely=.85, relheight=.03, relwidth=.05, anchor=CENTER)
+
+
+
+
+# finish implementing and assigning buttons and textbox to countdown function
+countdown(-1, True)
+
+
+
+
+
+
+
+root.after(500, lockdown.startlockdown)
+
 root.mainloop()
--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Pomoimprove
 required packages:
 pywin32
 
+
 **DO THIS TO MAKE SURE THE PROGRAM WILL WORK AFTER MERGING (only needs to be done once):**  
 Extract the Chrome.7z, place the "Chrome" folder (the one with chrome.exe) in the same directory as Chrome.7z (PROGRAM).
 
