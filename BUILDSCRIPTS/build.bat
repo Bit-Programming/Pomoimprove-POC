3ec3eb1a0e5cf97b58b5c98d5568a9e94394a628
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-11-14 14:37:02 -0700

    Update build scripts (now working)
    Updated .gitignore to include main.spec for building, build.bat now just points to main.spec which includes all the files, and main.spec was added which now has proper Chrome adding support. Well, I think, I still need to make sure Chrome is added and working. But, the program at least doesn't crash for now.

--- BUILDSCRIPTS/build.bat
+++ BUILDSCRIPTS/build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconsole --onefile --add-data="C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Chrome*;C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Chrome" -i "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\BUILDSCRIPTS\Pomoimprove.ico" "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Scripts\main.py"
+pyinstaller "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\BUILDSCRIPTS\main.spec"
