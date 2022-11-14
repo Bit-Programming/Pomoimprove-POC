9988dafb76c98710a73dc2f79dc4bb39e534eecf
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-11-14 13:40:28 -0700

    Include Chrome with build (NOT FUNCTIONAL)
    Include Chrome with the exe when building. It is not functional, fails when trying to extract Chrome seemingly.

--- BUILDSCRIPTS/build.bat
+++ BUILDSCRIPTS/build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconsole --onefile -i "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\BUILDSCRIPTS\Pomoimprove.ico" "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Scripts\main.py"
+pyinstaller --noconsole --onefile --add-data="C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Chrome*;C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Chrome" -i "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\BUILDSCRIPTS\Pomoimprove.ico" "C:\Users\UHDbits\Documents\GitHub\Pomoimprove\PROGRAM\Scripts\main.py"
