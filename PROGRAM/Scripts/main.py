d4c315cf71d4c0b49c7eb8b6d466406c5bee3f14
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-11-16 21:30:32 -0700

    Almost finalize main.py
    Improve organization. The only changes left are to maybe add the Pomoimprove logo, and to make sure it calls the right timer function.

--- PROGRAM/Scripts/main.py
+++ PROGRAM/Scripts/main.py
@@ -7,7 +7,8 @@ import lockdown, sys, os, stop, timer, sv_ttk
 # Enable HIDPI Support
 windll.shcore.SetProcessDpiAwareness(1)
 
-# Set Default Variables and Functions
+
+## Set Default Variables and Functions
 # This will give us the path of the application and the path of Chrome.
 if getattr(sys, 'frozen', False):
     application_path = sys._MEIPASS
@@ -15,10 +16,6 @@ else:
     os.chdir("..")
     application_path = os.getcwd()
 chrome_path = application_path + "\Chrome\chrome.exe"
-
-# Init Window
-root = Tk()
-sv_ttk.set_theme("dark")
 
 # Create popup about closing all windows
 def closeWindowsPopup():
@@ -28,13 +25,18 @@ def closeWindowsPopup():
         lockdown.secondlockdown()
         timer.countdown(5)
 
+
+## Init Window
+root = Tk()
+sv_ttk.set_theme("dark")
+
 # Adjust window settings
 root.title("Pomoimprove")
 root.resizable(False, False)
 root.overrideredirect(True)
+
 # Set window to be maximized by default
 root.state("zoomed")
-
 
 # Define document styles
 style = ttk.Style()
@@ -44,23 +46,20 @@ style.map(
     background=[("pressed", "!disabled", "black"), ("active", "white")],
 )
 
-# Code to add widgets will go here...
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-
-
 # Specify Grid
 Grid.rowconfigure(root, 0, weight=1)
 Grid.columnconfigure(root, 0, weight=1)
-
 Grid.rowconfigure(root, 1, weight=1)
 
+
+## Main Code
 # Create Buttons
-def on_enter(e):
+def on_enter(e): # Change color when hovered over
     button_2['background'] = '#61cbfc'
 
-def on_leave(e):
+def on_leave(e): # Revert color when no longer hovered over
     button_2['background'] = '#1c1c1c'
+
 button_2 = Button(root, borderwidth=1, text="Quit", height=3, width=10, command=stop.stop)
 button_2.bind("<Enter>", on_enter)
 button_2.bind("<Leave>", on_leave)
@@ -69,17 +68,12 @@ button_2.bind("<Leave>", on_leave)
 # Stick to left side
 button_2.grid(row=3, column=0, sticky="NSW")
 
-
 # Main Pomoimprove Logo
 Label(root, text="Pomoimprove", font=("Segoe UI", 30, "bold")).place(
     relx=0.5, rely=0.05, anchor=CENTER
 )
 
-# call countdown first time
-# root.after(0, countdown, 5)
-
-
-# Make the start/stop timer buttons
+# Make the "Start" Timer button
 Start = Button(
     root,
     bg="#3CC249",
@@ -91,11 +85,13 @@ Start = Button(
     command=lambda: closeWindowsPopup()
 ).place(relx=0.5, rely=0.80, relheight=0.06, relwidth=0.10, anchor=CENTER)
 
+# Close splash screen when app is loaded
 if '_PYIBoot_SPLASH' in os.environ:
     # NOT an import error
     import pyi_splash
-    pyi_splash.close()
+    pyi_splash.close
 
+# Run lockdown code after running application
 root.after(500, lockdown.firstlockdown)
 
 root.mainloop()
