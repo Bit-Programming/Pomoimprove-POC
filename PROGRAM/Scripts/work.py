f7b86d7dab3d201ef080aa84f35b66d3f8e5fa0d
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-11-21 12:13:23 -0700

    Add Spotify account popup
    Added a popup for them to sign into their Spotify account, also modified the Google account code.

--- PROGRAM/Scripts/work.py
+++ PROGRAM/Scripts/work.py
@@ -13,9 +13,21 @@ windll.shcore.SetProcessDpiAwareness(1)
 # Prompt for Google account signin
 def googleAccountPopup():
     message = tkinter.messagebox.showinfo("Google Account", "To proceed with this application, you must sign into your Google Account. Your Google Account will be signed out of once the experiment is concluded. After you sign in, please close out of the window.")
-    if message == 'ok':
+    # This code below will open the Chrome window, no matter what they answer to the question
+    if message != '':
+        # Start Chrome for person to signin
         DETACHED_PROCESS = 0x00000008
         subprocess.call(config.chrome_path + " --new-window --app=https://accounts.google.com", creationflags=DETACHED_PROCESS)
+# Prompt for Spotify account signin
+def spotifyAccountPopup():
+    message = tkinter.messagebox.askquestion("Spotify Account", "If you want to, you can sign into your Spotify account and play music in the background while you are working. This is known to further increase productivity. Once signed in, close out of the window.")
+    if message == 'yes':
+        # Set a variable so we know that they have a Spotify account
+        spotifyaccount = 1
+        DETACHED_PROCESS = 0x00000008
+        subprocess.call(config.chrome_path + " --new-window --app=https://accounts.spotify.com/en/login", creationflags=DETACHED_PROCESS)
+    else:
+        spotifyaccount = 0
 
 # Make function to quit out of the timer and Chrome
 def quit():
@@ -34,6 +46,8 @@ keyboard.add_hotkey('ctrl + alt + shift + b + i + t', quit)
 ## Main Code
 # Open prompt asking to sign into your Google Account
 googleAccountPopup()
+# Open prompt asking if you want to sign into your Spotify account
+spotifyAccountPopup()
 
 # Init Window
 root = Tk()
