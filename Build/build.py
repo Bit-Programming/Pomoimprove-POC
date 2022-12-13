29370b5535684f120184b0d0bcee4a6596f45a48
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-12-13 13:04:33 -0700

    Update language in build.py
    Change "This application" to "Pomoimprove" in build.py.

--- Build/build.py
+++ Build/build.py
@@ -6,10 +6,10 @@ os = platform.system()
 
 # Find the current OS, and run the pyinstaller command based on the OS, or say that the OS is not supported
 if os == "Darwin":
-	print("This application does not yet support macOS. There are plans to implement macOS support, but for now, this application only supports Windows.")
+	print("Pomoimprove does not yet support macOS. There are plans to implement macOS support, but for now, Pomoimprove only supports Windows.")
 elif os == "Linux":
-	print("This application does not yet support GNU/Linux. There are plans to implement GNU/Linux support, but for now, this application only supports Windows.")
+	print("Pomoimprove does not yet support GNU/Linux. There are plans to implement GNU/Linux support, but for now, Pomoimprove only supports Windows.")
 elif os == "Windows":
 	print("IMPLEMENT LATER")
 else:
-	print("This application does not support the operating system that you are running. Please use Windows for immediate support, or use macOS or GNU/Linux for eventual support.")
+	print("Pomoimprove does not support the operating system that you are running. Please use Windows for immediate support, or use macOS or GNU/Linux for eventual support.")
