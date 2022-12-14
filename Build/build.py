d7baf70aac5b75f1b6e0f58ddbed80a7b4303421
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-12-14 11:42:21 -0700

    Add “mac.spec”, remove “os” variable
    Added “mac.spec”, which will eventually be used for building on macOS, and also removed the “operatingsystem” variable, and replaced it with “platform.system()”.

--- Build/build.py
+++ Build/build.py
@@ -2,15 +2,12 @@ import platform
 import os
 
 
-# Set the "os" variable to the current OS
-operatingsystem = platform.system()
-
 # Find the current OS, and run the pyinstaller command based on the OS, or say that the OS is not supported
-if operatingsystem == "Darwin":
+if platform.system() == "Darwin":
 	print('Pomoimprove does not yet support macOS. There are plans to implement macOS support, but for now, Pomoimprove only supports Windows.')
-elif operatingsystem == "Linux":
+elif platform.system() == "Linux":
 	print('Pomoimprove does not yet support GNU/Linux. There are plans to implement GNU/Linux support, but for now, Pomoimprove only supports Windows.')
-elif operatingsystem == "Windows":
+elif platform.system() == "Windows":
 	print('Coming soon.')
 else:
 	print('Pomoimprove does not support the operating system that you are running. Please use Windows for immediate support, or use macOS or GNU/Linux for eventual support.')
