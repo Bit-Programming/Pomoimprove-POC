8f27b8ea68c57fcd5a0ceb91b6641c9a3ff4ddeb
UHDbits <66931349+UHDbits@users.noreply.github.com>
2022-12-14 11:21:56 -0700

    Move around some files
    Moved around some files, and edited some lines in build.py.

--- Build/build.py
+++ Build/build.py
@@ -1,15 +1,16 @@
 import platform
+import os
 
 
 # Set the "os" variable to the current OS
-os = platform.system()
+operatingsystem = platform.system()
 
 # Find the current OS, and run the pyinstaller command based on the OS, or say that the OS is not supported
-if os == "Darwin":
-	print("Pomoimprove does not yet support macOS. There are plans to implement macOS support, but for now, Pomoimprove only supports Windows.")
-elif os == "Linux":
-	print("Pomoimprove does not yet support GNU/Linux. There are plans to implement GNU/Linux support, but for now, Pomoimprove only supports Windows.")
-elif os == "Windows":
-	print("IMPLEMENT LATER")
+if operatingsystem == "Darwin":
+	print('Pomoimprove does not yet support macOS. There are plans to implement macOS support, but for now, Pomoimprove only supports Windows.')
+elif operatingsystem == "Linux":
+	print('Pomoimprove does not yet support GNU/Linux. There are plans to implement GNU/Linux support, but for now, Pomoimprove only supports Windows.')
+elif operatingsystem == "Windows":
+	print('Coming soon.')
 else:
-	print("Pomoimprove does not support the operating system that you are running. Please use Windows for immediate support, or use macOS or GNU/Linux for eventual support.")
+	print('Pomoimprove does not support the operating system that you are running. Please use Windows for immediate support, or use macOS or GNU/Linux for eventual support.')
